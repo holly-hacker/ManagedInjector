469ed838da7cc42677e01129c8c7f0eea246997a
HoLLy <dario.db@hotmail.com>
2021-04-02 21:32:48 +0200

    Initial commit

---
+++ ManagedInjector.CLI/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ManagedInjector.CLI
+{
+	internal static class Program
+	{
+		private static void Main(string[] args)
+		{
+			Console.WriteLine("Hello World!");
+		}
+	}
+}
